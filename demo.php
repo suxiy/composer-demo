301c4f7a4215f958454d2426c6252624fa958fa6
suxiyhytrdh1987 <steven.sun1@connext.com.cn>
2019-11-20 13:50:01 +0800

    2

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Flower\Rose;
+$obj = new Rose;
+$obj->desc();
+
+
--- src/Flower/Rose.php
+++ src/Flower/Rose.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Flower\Rose;
+namespace Flower;
 
 class Rose
 {
